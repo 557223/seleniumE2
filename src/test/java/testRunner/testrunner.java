5ffc7e5dfb89e09b2738c096fad93f4e7cc6a3b9
Aravind <Aravind@DESKTOP-K22TB83>
2021-01-16 16:02:11 +0530

    updated with extent report

--- src/test/java/testRunner/testrunner.java
+++ src/test/java/testRunner/testrunner.java
@@ -1,7 +1,12 @@
 package testRunner;
 
 
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -10,10 +15,21 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 features = "src/test/java/features",
 glue= "stepDefinitionFiles",
-plugin = { "pretty", "html:target/htmlreports" },
+plugin="com.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html",
 monochrome = true
 )
 
 public class testrunner {
 
+	@AfterClass
+	public static void setup()
+	{
+	Reporter.loadXMLConfig(new File("src/test/resources/extent-config.xml"));
+	//Reporter.setSystemInfo("Test User", System.getProperty("user.name"));
+	Reporter.setSystemInfo("User Name", "AJ");
+	Reporter.setSystemInfo("Application Name", "Test App ");
+	Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
+	Reporter.setSystemInfo("Environment", "Production");
+	Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
+	}
 }				
